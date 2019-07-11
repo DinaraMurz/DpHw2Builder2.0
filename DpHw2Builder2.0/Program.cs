b5b3a04c369ff1f7a56036fe93dbe77469f29ab9
Доллар Музыка <dollarmudic@gmail.com>
2019-07-11 14:48:16 +0600

    Добавьте файлы проекта.

---
+++ DpHw2Builder2.0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw2Builder2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
